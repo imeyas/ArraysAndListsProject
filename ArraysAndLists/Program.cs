9928eee034faa3137333354b41d0dc44019a27bd
Ebo Onwunali <ebo@njgeneral.com>
2022-01-19 03:14:38 -0500

    Finished code for this exercise

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -8,12 +9,46 @@ namespace ArraysAndLists
         {
             //TODO:
 
+            var myIntArray = new int[1,2,3,4,5,6,7,8,9,10];
+
             // Create an int Array and populate numbers 1-10
+
+            var evens = new List<int>();
+            var odds = new List<int>();
 
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+
+            for (int i = 0; i <= 17; i++)
+            {
+                //Console.WriteLine($"\nWhat number would you like to add?");
+                //var addThisNum = int.Parse(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    //var result = addThisNum;
+                    evens.Add(i);
+                }
+
+                else if (i % 2 != 0)
+                {
+                    //var result = addThisNum;
+                    odds.Add(i);
+                }
+
+                else
+                {
+                    Console.WriteLine($"\nInvalid input. Please try again");
+                }
+
+            }
+
+            Console.WriteLine($"\nEven numbers = {evens.Count}");
+
+            Console.WriteLine($"\nOdd numbers = {odds.Count}");
+
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -22,11 +57,27 @@ namespace ArraysAndLists
              * or the odds List
              */
 
-            /* Now using foeach or for loops,
-             * display each List of even and odd numbers
-             *
-             * Try to be creative in your display
-             */
+            int iOne = 0;
+            Console.WriteLine("\nEvens List:");
+            for (iOne = 1; iOne <= 17; iOne++)
+            {
+                if (iOne % 2 == 0)
+                {
+                    Console.Write(iOne + " ");
+                }
+
+            }
+
+            Console.WriteLine("\nOdds List:");
+            for (iOne = 1; iOne <= 17; iOne++)
+            {
+                if (iOne % 2 != 0)
+                {
+                    Console.Write(iOne + " ");
+                }
+            }
+
+
         }
     }
 }
